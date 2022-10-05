7d4e3e7fe2153c6d6b76edcb351dcaf5f4fdb7b6
ThekingMX1998 <chen1263754478@gmail.com>
2022-10-05 18:51:35 +0800

    Update rm-ovz.sh

--- File/rm-ovz.sh
+++ File/rm-ovz.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
+GREEN="\033[32m"
+RED="\033[31m" 
+BLUE="\033[34m"
+END="\033[0m"
+
 root_need(){
     if [[ $EUID -ne 0 ]]; then
-        echo -e “\033[31m ERROR:Please running with root \033[0m” 
+        echo -e "${RED}ERROR:Please running with root!${END}" 
         exit 1
     fi
 }
 
+echo -e "${GREEN}removing apache2${END}"
 apt-get --purge remove apache2 -y
-echo -e “\033[32m removing apache2 \033[0m”
 
+echo -e "${GREEN}removing apache2.2-common${END}"
 apt-get --purge remove apache2.2-common -y
-echo -e “\033[32m removing apache2.2-common \033[0m”
 
+echo -e "${GREEN}removing apache2-doc${END}"
 apt-get --purge remove apache2-doc -y 
-echo -e “\033[32m removing apache2-doc \033[0m”
 
+echo -e "${GREEN}removing apache2-utils${END}"
 apt-get --purge remove apache2-utils -y
-echo -e “\033[32m removing apache2-utils \033[0m”
 
-apt-get autoremove -y 
 
+echo -e "${GREEN}Delete files related to apache${END}""
 find /etc -name "*apache*" |xargs  rm -rf
 rm -rf /var/www
 rm -rf /etc/libapache2-mod-jk
 dpkg -l |grep apache2|awk '{print $2}'|xargs dpkg -P
 
-echo -e “\033[31m Finished \033[0m” 
+echo -e "${BLUE}Finished${END}" 
