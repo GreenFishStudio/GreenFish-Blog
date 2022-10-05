aba351568e307cefa3ff4052b8f210eab24f6f99
ThekingMX1998 <chen1263754478@gmail.com>
2022-10-05 18:33:10 +0800

    Update rm-ovz.sh

--- File/rm-ovz.sh
+++ File/rm-ovz.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
 apt-get --purge remove apache2
+echo removing apache2
 apt-get --purge remove apache2.2-common
+echo removing apache2.2-common
 apt-get --purge remove apache2-doc
+echo removing apache2-doc
 apt-get --purge remove apache2-utils
+echo removing apache2-utils
 apt-get autoremove
 find /etc -name "*apache*" |xargs  rm -rf
 rm -rf /var/www
 rm -rf /etc/libapache2-mod-jk
 dpkg -l |grep apache2|awk '{print $2}'|xargs dpkg -P
-# 使用本命令来查看apache2是否卸载干净
-dpkg -l | grep apache2
+echo -e “\033[31m Finished \033[0m” 
